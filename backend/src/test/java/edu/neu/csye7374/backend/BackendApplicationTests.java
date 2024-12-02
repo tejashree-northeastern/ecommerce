2452868615ec3790672eb152b9f69eccf3e9a6db
Tejashree Gore <gore.te@northeastern.edu>
2024-12-01 20:16:25 -0500

    Add sample test

---
+++ backend/src/test/java/edu/neu/csye7374/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
