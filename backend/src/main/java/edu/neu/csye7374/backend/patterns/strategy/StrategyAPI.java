e05986865d5ce08bf185333bcb13e2f6b011189d
atharvakonge <atharvakonge98@gmail.com>
2024-12-01 19:32:28 -0500

    Added strategy design pattern logic

---
+++ backend/src/main/java/edu/neu/csye7374/backend/patterns/strategy/StrategyAPI.java
@@ -0,0 +1,11 @@
+package com.neu.design.pattern.project.ECommercePlatform.patterns.strategy;
+
+public interface StrategyAPI {
+
+        void add();
+
+        void update(int id);
+
+        void delete();
+
+    }
